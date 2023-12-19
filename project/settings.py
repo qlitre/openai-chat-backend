3be32eab1cb2a78dd3676d1eb714acbc4c7865ee
qlitre <kuri0403@gmail.com>
2023-12-19 23:22:41 +0900

    fixed bug

--- project/settings.py
+++ project/settings.py
@@ -16,10 +16,14 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost'] + [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['localhost']
+tmp = os.environ.get('ALLOWED_HOST')
+if tmp:
+    a_list = tmp.split(',')
+    for a in a_list:
+        ALLOWED_HOSTS.append(a)
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -145,14 +149,24 @@ DJOSER = {
 }
 
 CORS_ORIGIN_WHITELIST = [
-                            'http://127.0.0.1:3000',
-                            'http://localhost:3000',
-                        ] + [os.environ.get('CORS_ORIGIN_WHITELIST')]
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
+tmp = os.environ.get('CORS_ORIGIN_WHITELIST')
+if tmp:
+    a_list = tmp.split(',')
+    for a in a_list:
+        CORS_ORIGIN_WHITELIST.append(a)
 
 CORS_ALLOWED_ORIGINS = [
-                           "http://localhost:3000",
-                           "http://127.0.0.1:3000",
-                       ] + [os.environ.get('CORS_ALLOWED_ORIGINS')]
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+tmp = os.environ.get('CORS_ALLOWED_ORIGINS')
+if tmp:
+    a_list = tmp.split(',')
+    for a in a_list:
+        CORS_ALLOWED_ORIGINS.append(a)
 
 # When you using credential include in the frontend must include below line
 CORS_ALLOW_CREDENTIALS = True
@@ -170,7 +184,13 @@ EMAIL_USE_TLS = True
 SESSION_COOKIE_AGE = 1800  # 30 Min
 
 # It will resolve error Forbidden (Origin checking failed - http://127.0.0.1:3000 does not match any trusted origins.)
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:3000'] + [os.environ.get('CSRF_TRUSTED_ORIGINS')]
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:3000', 'http://localhost:3000']
+tmp = os.environ.get('CSRF_TRUSTED_ORIGINS')
+if tmp:
+    a_list = tmp.split(',')
+    for a in a_list:
+        CSRF_TRUSTED_ORIGINS.append(a)
 
 # default is 31449600 seconds = 1 year approx
 CSRF_COOKIE_AGE = 60 * 60 * 24 * 14  # 14 days
